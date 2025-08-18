7d39a64ae335e68b70d8a84dd5d9201283ece39f
Mateusz Randak <ran.mateusz@gmail.com>
2025-08-18 21:40:04 +0200

    change harpoon keys

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -6,10 +6,10 @@ return {
     local harpoon = require 'harpoon'
     harpoon:setup {}
 
-    vim.keymap.set('n', '<leader>HA', function()
+    vim.keymap.set('n', '<leader>ha', function()
       harpoon:list():add()
     end, { desc = '[H]arpoon: [A]dd file' })
-    vim.keymap.set('n', '<leader>HL', function()
+    vim.keymap.set('n', '<leader>hl', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end, { desc = '[H]arpoon: [L]ist files' })
 
